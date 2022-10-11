8e3d506fe2912183373b40ea3ef289b0dbf80012
arianneelise <arianne@devpipeline.com>
2022-10-11 15:22:42 -0600

    fixed bug in add_friend, added init for util

--- movie/endpoints/add_friend.py
+++ movie/endpoints/add_friend.py
@@ -1,26 +1,27 @@
 import flask 
 from flask import jsonify
-from movie.models import Users, user_schema, Friends, friend_schema
-from movie.util import verify_user
+from movie.models import Friends, friend_schema
+from movie import util
 from movie import db
+from sqlalchemy import or_
 
 def add_friend(req:flask.Request) -> flask.Response:
   response = req.get_json()
   user_one_id = response.get("user_one_id")
   user_two_id = response.get("user_two_id")
 
-  if verify_user.verify_user(user_one_id) and verify_user.verify_user(user_two_id):
-    friend_check = db.session.query(Friends).filter(Friends.user_one_id == user_one_id).all()
+  if util.verify_user(user_one_id) and util.verify_user(user_two_id):
+    friend_check = db.session.query(Friends).filter(or_(Friends.user_one_id == user_one_id, Friends.user_two_id == user_one_id)).all()
 
     for item in friend_check:
       if str(item.user_one_id) == user_two_id or str(item.user_two_id) == user_two_id:
         return jsonify("Record already exists"), 400
 
-      new_friends = Friends(user_one_id, user_two_id)
-      db.session.add(new_friends)
-      db.session.commit()
+    new_friends = Friends(user_one_id, user_two_id)
+    db.session.add(new_friends)
+    db.session.commit()
 
-      return jsonify(friend_schema.dump(new_friends)), 200
+    return jsonify(friend_schema.dump(new_friends)), 200
 
   else:
     return jsonify("One or more invalid ids"), 400
