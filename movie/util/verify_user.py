c44825532a2e52de5a3e5552925c59fe42545c70
arianneelise <arianne@devpipeline.com>
2022-10-11 14:52:29 -0600

    verify_user now checks for uuid, and add_friend now includes working error catching

--- movie/util/verify_user.py
+++ movie/util/verify_user.py
@@ -1,11 +1,16 @@
+from uuid import UUID
 import flask 
 from flask import jsonify
 from movie.models import Users, user_schema
 from movie import db
 
 def verify_user(id):
-  user_check = db.session.query(Users).filter(Users.id == id).first()
-  if user_check:
-    return True
-  else:
+  try:
+    verify_id = UUID(id, version=4)
+    user_check = db.session.query(Users).filter(Users.id == verify_id).first()
+    if user_check:
+      return True
+    else:
+      return False
+  except:
     return False
